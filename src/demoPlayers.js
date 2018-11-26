c7063f638b56060008081c7f171001c0ef154b43
Lucas Pradel <lucaspradel@gmail.com>
2018-11-26 19:31:45 +0100

    chore: Put AddPlayerInput into its own component

---
+++ src/demoPlayers.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    playerInfo: {
+      name: "Luka Doncic",
+      team: "Dallas Mavericks",
+      position: "G/F",
+      age: 19.8,
+      image:
+        "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/1610612742/2018/260x190/1629029.png"
+    },
+    playerStats: {
+      pointsPerGame: 20.2,
+      reboundsPerGame: 6.3,
+      assistsPerGame: 4.5,
+      effectiveFieldGoalPercentage: 59.1,
+      stealsPerGame: 0.9,
+      blocksPerGame: 1.1,
+      threePointPercentage: 39.1,
+      freeThrowPercentage: 75.1
+    }
+  },
+  {
+    playerInfo: {
+      name: "Lebron James",
+      team: "Los Angeles Lakers",
+      position: "G/F",
+      age: 35.1,
+      image:
+        "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/1610612747/2018/260x190/2544.png"
+    },
+    playerStats: {
+      pointsPerGame: 28.2,
+      reboundsPerGame: 8.3,
+      assistsPerGame: 8.5,
+      effectiveFieldGoalPercentage: 63.1,
+      stealsPerGame: 1.9,
+      blocksPerGame: 1.9,
+      threePointPercentage: 34.1,
+      freeThrowPercentage: 75.1
+    }
+  }
+];
